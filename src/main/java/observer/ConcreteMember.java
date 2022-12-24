23bc5617b36b53a3fe2f1580406ca09a39c5d249
Jonatan Borezki <jonatan250199@gmail.com>
2022-12-24 19:35:31 +0200

    Java doc added in

---
+++ src/main/java/observer/ConcreteMember.java
@@ -0,0 +1,34 @@
+package observer;
+
+/**
+ * this class is an implement of Member interface which describes the recipient of the updates (Observer).
+ * The class contains a copy (copy sallow) of the shared UndoableStringBuilder.
+ * This class includes an update method to notify the member on the last change done to the UndoableStringBuilder.
+ * @author Jonatan Boritsky : 207254194,  Ilan Meyer Souffir : 342615648
+ */
+
+public class ConcreteMember implements Member {
+    private String name; // the name of the member
+    private UndoableStringBuilder usb; // the copy of the shared UndoableStringBuilder
+    private String lastChange; // the last change done to the UndoableStringBuilder
+
+    /**
+     * Constructor
+     * @param name - Given name of the member
+     */
+    public ConcreteMember(String name) {
+        this.name = name;
+        this.lastChange = "";
+    }
+
+    /**
+     * This method updates the member on the last change done to the UndoableStringBuilder.
+     * @param usb - the shared UndoableStringBuilder
+     */
+    @Override
+    public void update(UndoableStringBuilder usb) {
+        System.out.println("Member " + this.name + " was notified of the change: " + this.lastChange + " -> " + usb.toString());
+        this.lastChange = usb.toString();
+        this.usb = usb;
+    }
+}
---
+++ src/main/java/observer/GroupAdmin.java
@@ -0,0 +1,106 @@
+package observer;
+import java.util.HashSet;
+
+/**
+ * This class is an implement of Sender interface, which describes the sender of the updates (Observable) .
+ * This class contains the states pool (UndoableStringBuilder)
+ * The class includes a list of observers (members) who should receive updates on any change made to the data.
+ * The list of observers is implemented with a HashSet.
+ * The class includes methods to register and unregister observers, and contains calls to the UndoableStringBuilder methods.
+ * @author Jonatan Boritsky : 207254194,  Ilan Meyer Souffir : 342615648
+ */
+
+public class GroupAdmin implements Sender {
+    private HashSet<Member> observers; // the list of observers (members)
+    private UndoableStringBuilder usb; // the states pool (UndoableStringBuilder)
+
+    /**
+     * Constructor
+     * @param usb - the states pool (UndoableStringBuilder)
+     */
+    public GroupAdmin(UndoableStringBuilder usb) {
+        this.observers = new HashSet<Member>();
+        this.usb = usb;
+    }
+
+    /**
+     * This method registers a new observer (member) to the list of observers.
+     * Check if the observer is already in the list, if not, add it.
+     * @param obj - the new observer (member)
+     */
+    @Override
+    public void register(Member obj) {
+        if(this.observers.contains(obj)){
+            System.out.println("This observer has already registered");
+        }
+        else{
+            obj.update(usb);
+            this.observers.add(obj);
+        }
+    }
+
+    /**
+     * This method unregisters an observer (member) from the list of observers.
+     * Check if the observer is in the list, if so, remove it.
+     * @param obj - the observer (member) to unregister
+     */
+    @Override
+    public void unregister(Member obj) {
+        if(this.observers.contains(obj)) {
+            this.observers.remove(obj);
+            obj.update(null);
+        }
+        else{
+            System.out.println("This observer has not registered");
+        }
+    }
+
+    /**
+     * This method inserts the string into mutable characters sequence.
+     * @param offset - the offset where we start to insert
+     * @param obj - the string to insert
+     */
+    @Override
+    public void insert(int offset, String obj) {
+        this.usb.insert(offset, obj);
+        for (Member obs : this.observers) {
+            obs.update(this.usb);
+        }
+    }
+
+    /**
+     * This method appends the required string to the mutable characters sequence.
+     * @param obj - the string to append
+     */
+    @Override
+    public void append(String obj) {
+        this.usb.append(obj);
+        for (Member obs : this.observers) {
+            obs.update(this.usb);
+        }
+    }
+
+    /**
+     * This method removes the characters from specified range of this sequence.
+     * @param start - the start index of the substring to delete
+     * @param end - the end index of the substring to delete
+     */
+    @Override
+    public void delete(int start, int end) {
+        this.usb.delete(start, end);
+        for (Member obs : this.observers) {
+            obs.update(this.usb);
+        }
+    }
+
+    /**
+     * The method come back to the previous state of the UndoableStringBuilder.
+     */
+    @Override
+    public void undo() {
+        this.usb.undo();
+        for (Member obs : this.observers) {
+            obs.update(this.usb);
+        }
+    }
+}
